21a70273230fe357507b7bc31d609849ea0ae8b4
Carlos Ramirez <ramirezcoronelcarlos@gmail.com>
2019-12-15 16:56:11 -0400

    added-button

--- js/jQueryTG4.js
+++ js/jQueryTG4.js
@@ -8,11 +8,16 @@ $(document).ready(function (){
 //Closure para reutilizar un contador
 function carrera (el) {
   let contador = 0
+  let id = $(el).attr('id').slice(-1) //obtengo el ultimo numero de su id
 
   //cuando den click en el boton
   $(el).click(function() {
     seleccionDeCarro()
   })
+
+  $('#bOk' + id).click(function () ) {
+    
+  }
 
   //Recorrer array de carros y mostrar previsualizacion
   function seleccionDeCarro() {
@@ -24,7 +29,6 @@ function carrera (el) {
     cambiarImagen( $('#miCarrera'), $(nuevoSrc).attr('src') )
     contador++
   }
-
 }
 
 function cambiarImagen(el, src) {
